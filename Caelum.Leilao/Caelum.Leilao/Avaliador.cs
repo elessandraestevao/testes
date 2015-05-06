08b135b20d4844fe93d81d1ec013c3439c53a1b9
Elessandra Estevao <elessandraestevao@hotmail.com>
2015-05-06 18:58:00 -0300

    Teste de Unidade com NUnit - 1

---
+++ Caelum.Leilao/Caelum.Leilao/Avaliador.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caelum.Leilao
+{
+    public class Avaliador
+    {
+        private double maiorValor = double.MinValue;
+        private double menorValor = double.MaxValue;
+
+        public void Avalia(Leilao leilao)
+        {
+            foreach (var l in leilao.Lances)
+            {
+                if (l.Valor > maiorValor)
+                {
+                    maiorValor = l.Valor;
+                }
+                if (l.Valor < menorValor)
+                {
+                    menorValor = l.Valor;
+                }
+            }
+        }
+
+        public double MaiorLance
+        {
+            get { return maiorValor; }
+        }
+
+        public double MenorLance
+        {
+            get {return menorValor;}
+        }
+    }
+}
---
+++ Caelum.Leilao/Caelum.Leilao/AvaliadorTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+namespace Caelum.Leilao
+{
+    [TestFixture]
+    public class AvaliadorTest
+    {
+        [Test]
+        public void DeveEntenderLancesEmOrdemCrescente()
+        {
+            //cenário
+            Leilao leilao = new Leilao("Primeiro Leilão");
+
+            Usuario joao = new Usuario("João");
+            Usuario jose = new Usuario("José");
+            Usuario maria = new Usuario("Maria");
+
+            leilao.Propoe(new Lance(maria, 200.0));            
+            leilao.Propoe(new Lance(jose, 300.0));
+            leilao.Propoe(new Lance(joao, 400.0));
+            
+
+            //Ação
+            Avaliador avaliador = new Avaliador();
+            avaliador.Avalia(leilao);
+
+            //Validação
+            double menorValorEsperado = 200;
+            double maiorValorEsperado = 400;
+
+            Assert.AreEqual(menorValorEsperado, avaliador.MenorLance);
+            Assert.AreEqual(maiorValorEsperado, avaliador.MaiorLance);
+        }
+
+        [Test]
+        public void DeveAgirComNumerosNegativos()
+        {
+
+        }
+    }
+}
